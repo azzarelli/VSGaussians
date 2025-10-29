aea1a3957d9a097484ae13c1eef705cf79d952cd
azzarelli <aazzarellib@gmail.com>
2025-10-29 16:02:25 +0000

    update slurm job

--- gpu_jobn.sh
+++ gpu_jobn.sh
@@ -9,12 +9,13 @@
 module load cuda/11.8
 
 source ~/miniforge3/bin/activate
-conda create -n vsenv python=3.10 -y
+# conda create -n vsenv python=3.10 -y
 conda activate vsenv
-srun nvidia-smi --list-gpus
+
 # pip install torch==2.4.0 torchvision==0.19.0 --index-url https://download.pytorch.org/whl/cu118
 # pip install torch==2.0.1 torchvision==0.15.2 --index-url https://download.pytorch.org/whl/cu118
-pip install torch==2.0.1+cu118 torchvision==0.15.2+cu118 --index-url https://download.pytorch.org/whl/cu118
+pip install torch==2.0.1+cu118 torchvision==0.15.2+cu118 \
+  --extra-index-url https://download.pytorch.org/whl/cu118
 python -c "import torch; print(torch.__version__); print(torch.version.cuda); print(torch.cuda.is_available())"
 
 # SAVEDIR=/studio4-1/studio4-1/
