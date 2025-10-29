5683e96e7ac916e200e5130b9809942f955598a8
azzarelli <aazzarellib@gmail.com>
2025-10-29 14:48:51 +0000

    update slurm job

--- gpu_jobn.sh
+++ gpu_jobn.sh
@@ -3,9 +3,18 @@
 #SBATCH --job-name=docs_ex3
 #SBATCH --output=docs_ex3.out
 #SBATCH --gpus=1                # this allocates 72 CPU cores
-#SBATCH --ntasks-per-gpu=3
+#SBATCH --ntasks-per-gpu=1
 #SBATCH --time=00:5:00
 
-module load cray-python
+module load cuda/11.8
+module load cudatoolkit/24.11_11.8
 
-srun python3 python_test.py
+
+SAVEDIR=/studio4-1/studio4-1/
+EXP_NAME=test
+
+echo "Training starting..."
+srun python gui.py -s "$SAVEDIR" \
+  --expname "$SAVEDIR/$EXP_NAME" \
+  --configs arguments/studio4.py \
+  --test_iterations 2000
