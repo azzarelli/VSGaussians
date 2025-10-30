6c4463ad1a3e69b3b3c2b1501ca4ed0407bcfca6
azzarelli <aazzarellib@gmail.com>
2025-10-30 14:26:38 +0000

    slurm

--- gpu_jobn.sh
+++ gpu_jobn.sh
@@ -4,20 +4,20 @@
 #SBATCH --output=docs_ex3.out
 #SBATCH --gpus=1
 #SBATCH --ntasks-per-gpu=1
-#SBATCH --time=00:10:00
+#SBATCH --time=00:40:00
 
-module load cuda/11.8
+
+module load cray-python
+module load cudatoolkit
 
 source ~/miniforge3/bin/activate
-# conda create -n vsenv python=3.10 -y
 conda activate vsenv
+# pip install torch torchvision --index-url https://download.pytorch.org/whl/cu126
+# pip install wheel
+# TORCH_CUDA_ARCH_LIST="9.0" CC=gcc-13 CXX=g++-13 pip install --no-build-isolation git+https://github.com/nerfstudio-project/gsplat
 
-# PIP not working
-# pip install torch==2.4.0 torchvision==0.19.0 --index-url https://download.pytorch.org/whl/cu118
-# pip install torch==2.0.1 torchvision==0.15.2 --index-url https://download.pytorch.org/whl/cu118
-# pip install torch==2.1.0+nv23.10 torchvision==0.16.0+nv23.10 \
-#     --extra-index-url https://pypi.nvidia.com
-python -c "import torch; print(torch.__version__); print(torch.version.cuda); print(torch.cuda.is_available())"
+pip install -r requirements.txt
+# python python_test.py
 
 
 # SAVEDIR=/studio4-1/studio4-1/
