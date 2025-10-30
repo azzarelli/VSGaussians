1721b1d71fcfd5b53a165831aff14592a2b3e8e3
azzarelli <aazzarellib@gmail.com>
2025-10-30 15:42:06 +0000

    slurm

--- gpu_jobn.sh
+++ gpu_jobn.sh
@@ -6,6 +6,11 @@
 #SBATCH --ntasks-per-gpu=1
 #SBATCH --time=08:00:00
 
+# Skip the leading "--"
+if [[ $1 == "--" ]]; then
+  shift
+fi
+
 ARGS=$1
 EXP_NAME=$2
 
@@ -15,9 +20,12 @@ module load cudatoolkit
 source ~/miniforge3/bin/activate
 conda activate vsenv
 
+export OPENBLAS_NUM_THREADS=1
+export OMP_NUM_THREADS=1
+
 SAVEDIR=$HOME/data/studio4-1/
 echo "Training starting..."
-python gui.py -s "$SAVEDIR" \
+srun python gui.py -s "$SAVEDIR" \
   --expname "$SAVEDIR/$EXP_NAME" \
-  --configs arguments/$ARGS \
+  --configs "arguments/$ARGS" \
   --test_iterations 4000
